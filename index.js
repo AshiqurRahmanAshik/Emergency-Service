73d0944eb67ac65bed7460d966d2b5fbefccac3e
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-08-26 23:53:01 +0600

    Functional Call Button With Alert Added

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-// Card Heart Icon Functionalities
 const cardContainer = document.getElementById("card-container");
 cardContainer.addEventListener("click", function (e) {
+  // Card Heart Icon Functionalities
   if (e.target.className.includes("fa-heart")) {
     const totalHeart = document.getElementById("total-heart").innerText;
     const updatedHeart = Number(totalHeart) + 1;
     document.getElementById("total-heart").innerText = updatedHeart;
   }
+  // Service Name & Service Number Alert
+  const callBtn = e.target.closest(".call-btn");
+  if (callBtn) {
+    const serviceName =
+      callBtn.parentNode.parentNode.children[1].children[0].innerText;
+    const serviceNumber =
+      callBtn.parentNode.parentNode.children[1].children[2].innerText;
+    alert(`📞Calling ${serviceName} ${serviceNumber}`);
+  }
 });
+console.log("Live");
