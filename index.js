37064e9cd4c4b5d3a1c135da95adb3deefa880c9
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-08-28 23:25:21 +0600

    Emergency Service Final Project

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const cardContainer = document.getElementById("card-container");
-
 cardContainer.addEventListener("click", function (e) {
   //  Heart functionality
-  if (e.target.className.includes("fa-heart")) {
+  const heartBtn = e.target.className.includes("fa-heart");
+  if (heartBtn) {
     const totalHeart = document.getElementById("total-heart").innerText;
     const updatedHeart = Number(totalHeart) + 1;
     document.getElementById("total-heart").innerText = updatedHeart;
@@ -19,7 +19,6 @@ cardContainer.addEventListener("click", function (e) {
     const totalCoin = document.getElementById("total-coin").innerText;
     const totalCoinNumber = Number(totalCoin);
     const NetCoinNumber = totalCoinNumber - 20;
-
     if (totalCoinNumber < 20) {
       alert(
         `❌ You don't have sufficient coin. You need at least 20 coins to call`
@@ -57,13 +56,12 @@ cardContainer.addEventListener("click", function (e) {
   if (copyBtn) {
     const copyServiceName =
       copyBtn.parentNode.parentNode.children[1].children[1].innerText;
-
     const copyServiceNumber =
       copyBtn.parentNode.parentNode.children[1].children[2].innerText;
     alert(
       `${copyServiceName} Number has been copied & the number is ${copyServiceNumber}`
     );
-    navigator.clipboard.writeText(copyServiceNumber).then(() => {});
+    navigator.clipboard.writeText(copyServiceNumber);
 
     const totalCopy = document.getElementById("total-copy").innerText;
     const updatedCopy = Number(totalCopy) + 1;
